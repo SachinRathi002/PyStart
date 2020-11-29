cfbd2365067cf384f96d2ee0543106a66bc96e68
SachinRathi002 <sachin.avertispharma@gmail.com>
2020-11-29 07:39:15 +0530

    change into added URLs for shoppings

--- auto_webservice.py
+++ auto_webservice.py
@@ -7,6 +7,7 @@ social_urls = ["https://twitter.com/", "www.facebook.com", "https://www.instagra
 news_urls = ["https://www.bbc.com/", "https://edition.cnn.com/world", "https://timesofindia.indiatimes.com/",
              "http://ddnews.gov.in/", "https://www.thehindu.com/"]
 entrtmnt_urls = ["www.youtube.com", "www.netflix.com", "https://www.primevideo.com/"]
+e_comm_urls = ["https://www.amazon.in/", "https://www.flipkart.com/"]
 
 def open_tabs(urls_lists):
 	for i in urls_lists:
@@ -24,12 +25,16 @@ def main_news():
 def main_entrtmnt():
 	open_tabs(entrtmnt_urls)
 
+def main_ecomm():
+    open_tabs(e_comm_urls)
+
 print("Hello Good Moring!!!")
 print("""Making your day more shiny....
 Press ->1 for Search
 Press ->2 for Social Media
 Press ->3 for News
-Press ->4 for Entertaiment""")
+Press ->4 for Entertaiment
+Press ->5 for eStore""")
 userInput = int(input("Please enter your choice: "))
 print("As you said we did it for you :)...")
 
@@ -41,5 +46,7 @@ elif(userInput == 3):
     main_news()
 elif(userInput == 4):
     main_entrtmnt()
+elif(userInput == 5):
+    main_ecomm()
 else:
     print("Invalid option")
